1578d99faf7259a79876c2bc6049152027fcb0ed
lleoon <96005316+justlleoon@users.noreply.github.com>
2024-10-08 14:47:06 +0800

    QUICK FIX 1.1.0.2

--- apricus-bmi/main.js
+++ apricus-bmi/main.js
@@ -20,16 +20,16 @@ function calculateBMI() {
         $("#results p").html("Your BMI is considered <b>heathly</b>. We reccomend to stay active and maintain a healthy diet.")
     } else if (bmi >= 24.9 && bmi < 29.9 ) {
         document.getElementById("results").style.backgroundColor = "orange";
-        $("#results p").html("Your BMI is considered <b>overweight</b>. Start exercising regularly and have a managed diet.")
+        $("#results p").html("Your BMI is considered <b>overweight</b>. Exercise regularly and have a managed diet.")
     } else if (bmi >= 29.9 && bmi < 34.9){
         document.getElementById("results").style.backgroundColor = "red";
-        $("#results p").html("Your BMI is considered <b>obese ( class I )</b> Start exercising more often and have a managed diet. Doing so can reduce chances of diseases.")
+        $("#results p").html("Your BMI is considered <b>obese ( class I )</b> Exercise more often and have a managed diet. Doing so can reduce chances of diseases.")
     } else if (bmi >= 34.9 && bmi < 39.9){
         document.getElementById("results").style.backgroundColor = "red";
-        $("#results p").html("Your BMI is considered <b>obese ( class II )</b> Start exercising more often and have a managed diet. Doing so can reduce chances of diseases.")
+        $("#results p").html("Your BMI is considered <b>obese ( class II )</b> Exercise more often and have a managed diet. Doing so can reduce chances of diseases.")
     } else {
         document.getElementById("results").style.backgroundColor = "red";
-        $("#results p").html("Your BMI is considered <b>obese ( class III )</b> It's very important to start exercising more often and have a managed diet. Doing so can reduce chances of diseases.")
+        $("#results p").html("Your BMI is considered <b>obese ( class III )</b> Exercise more often and have a managed diet. Doing so can reduce chances of diseases.")
     }
     $("#results h1").text(`Your BMI is ${bmi}.`)
     $("#results").slideDown()
